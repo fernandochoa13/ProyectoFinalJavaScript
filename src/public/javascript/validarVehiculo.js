95f4cd997b29465edf86c01bf334c0ab53c6378a
Fernando Ochoa <113475469+fernandochoa13@users.noreply.github.com>
2023-07-26 21:15:17 -0400

    Update validarVehiculo.js
    wee

---
+++ src/public/javascript/validarVehiculo.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    formulario = document.getElementById("formularioVehiculo");
+      formulario.addEventListener('submit', validarFormulario); 
+    });
+  
+    
+    
+    function validarFormulario(evento) {
+      evento.preventDefault();
+      var marca = document.getElementById('marca').value;
+      if(marca.length == 0) {
+        alert('No has escrito nada en la marca');
+        return;
+      }
+      var color = document.getElementById('color').value;
+      if (color.length < 6) {
+        alert('No has escrito nada en el color');
+        return;
+      }
+      var modelo = document.getElementById('modelo').value;
+      if(modelo.length == 0) {
+        alert('No has escrito nada en el modelo');
+        return;
+      }
+      this.submit();
+    }
